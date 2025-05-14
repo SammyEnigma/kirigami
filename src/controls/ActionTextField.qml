SammyEnigma/kirigami

commit d8cbb0fd050861a0e08fd45192d311b65d2d4dc3

PC3/TextField,SearchField: Fix mirroring detection

src/controls/ActionTextField.qml
@@ -73,12 +73,11 @@ Controls.TextField {
 
     hoverEnabled: true
 
-    horizontalAlignment: Qt.AlignLeft
-    LayoutMirroring.enabled: Qt.application.layoutDirection === Qt.RightToLeft
-    LayoutMirroring.childrenInherit: true
+    // Manually setting this fixes alignment in RTL layouts
+    horizontalAlignment: TextInput.AlignLeft
 
-    leftPadding: Kirigami.Units.smallSpacing + (LayoutMirroring.enabled ? rightActionsRow : leftActionsRow).width
-    rightPadding: Kirigami.Units.smallSpacing + (LayoutMirroring.enabled ? leftActionsRow : rightActionsRow).width
+    leftPadding: Kirigami.Units.smallSpacing + (root.effectiveHorizontalAlignment === TextInput.AlignRight ? rightActionsRow : leftActionsRow).width
+    rightPadding: Kirigami.Units.smallSpacing + (root.effectiveHorizontalAlignment === TextInput.AlignRight ? leftActionsRow : rightActionsRow).width
 
     Behavior on leftPadding {
         NumberAnimation {
@@ -114,6 +113,7 @@ Controls.TextField {
         padding: Kirigami.Units.smallSpacing
         spacing: Kirigami.Units.smallSpacing
         layoutDirection: Qt.LeftToRight
+        LayoutMirroring.enabled: root.effectiveHorizontalAlignment === TextInput.AlignRight
         anchors.left: parent.left
         anchors.leftMargin: Kirigami.Units.smallSpacing
         anchors.top: parent.top
@@ -147,6 +147,7 @@ Controls.TextField {
         padding: Kirigami.Units.smallSpacing
         spacing: Kirigami.Units.smallSpacing
         layoutDirection: Qt.RightToLeft
+        LayoutMirroring.enabled: root.effectiveHorizontalAlignment === TextInput.AlignRight
         anchors.right: parent.right
         anchors.rightMargin: Kirigami.Units.smallSpacing
         anchors.top: parent.top

src/controls/SearchField.qml
@@ -60,12 +60,12 @@ Kirigami.ActionTextField {
     property bool delaySearch: false
 
     // padding to accommodate search icon nicely
-    leftPadding: if (Qt.application.layoutDirection === Qt.RightToLeft) {
+    leftPadding: if (effectiveHorizontalAlignment === TextInput.AlignRight) {
         return _rightActionsRow.width + Kirigami.Units.smallSpacing
     } else {
         return (activeFocus || root.text.length > 0 ? 0 : (searchIcon.width + Kirigami.Units.smallSpacing)) + Kirigami.Units.smallSpacing * 2
     }
-    rightPadding: if (Qt.application.layoutDirection === Qt.RightToLeft) {
+    rightPadding: if (effectiveHorizontalAlignment === TextInput.AlignRight) {
         return (activeFocus || root.text.length > 0 ? 0 : (searchIcon.width + Kirigami.Units.smallSpacing)) + Kirigami.Units.smallSpacing * 2
     } else {
         return _rightActionsRow.width + Kirigami.Units.smallSpacing
@@ -74,6 +74,7 @@ Kirigami.ActionTextField {
     Kirigami.Icon {
         id: searchIcon
         opacity: root.activeFocus || text.length > 0 ? 0 : 1
+        LayoutMirroring.enabled: root.effectiveHorizontalAlignment === TextInput.AlignRight
         anchors.left: root.left
         anchors.leftMargin: Kirigami.Units.smallSpacing * 2
         anchors.verticalCenter: root.verticalCenter
@@ -101,7 +102,8 @@ Kirigami.ActionTextField {
     inputMethodHints: Qt.ImhNoPredictiveText
     rightActions: [
         Kirigami.Action {
-            icon.name: root.LayoutMirroring.enabled ? "edit-clear-locationbar-ltr" : "edit-clear-locationbar-rtl"
+            //ltr confusingly refers to the direction of the arrow in the icon, not the text direction which it should be used in
+            icon.name: root.effectiveHorizontalAlignment === TextInput.AlignRight ? "edit-clear-locationbar-ltr" : "edit-clear-locationbar-rtl"
             visible: root.text.length > 0
             onTriggered: {
                 root.clear();

commit 5bf798c37c712bd36abdb5eb9ed70be21975d534

Fix ActionTextField RTL
This got broken in d8cbb0fd050861a0e08fd45192d311b65d2d4dc3, which was
not technically correct; if it worked at the time, it broke since then.

This reverts d8cbb0fd050861a0e08fd45192d311b65d2d4dc3

BUG: 504091
FIXED-IN: 6.15

src/controls/ActionTextField.qml
@@ -79,11 +79,12 @@ QQC2.TextField {
 
     hoverEnabled: true
 
-    // Manually setting this fixes alignment in RTL layouts
-    horizontalAlignment: TextInput.AlignLeft
+    horizontalAlignment: Qt.AlignLeft
+    LayoutMirroring.enabled: Qt.application.layoutDirection === Qt.RightToLeft
+    LayoutMirroring.childrenInherit: true
 
-    leftPadding: Kirigami.Units.smallSpacing + (root.effectiveHorizontalAlignment === TextInput.AlignRight ? rightActionsRow : leftActionsRow).width
-    rightPadding: Kirigami.Units.smallSpacing + (root.effectiveHorizontalAlignment === TextInput.AlignRight ? leftActionsRow : rightActionsRow).width
+    leftPadding: Kirigami.Units.smallSpacing + (LayoutMirroring.enabled ? rightActionsRow : leftActionsRow).width
+    rightPadding: Kirigami.Units.smallSpacing + (LayoutMirroring.enabled ? leftActionsRow : rightActionsRow).width
 
     Behavior on leftPadding {
         NumberAnimation {
@@ -164,7 +165,6 @@ QQC2.TextField {
         padding: Kirigami.Units.smallSpacing
         spacing: Kirigami.Units.smallSpacing
         layoutDirection: Qt.LeftToRight
-        LayoutMirroring.enabled: root.effectiveHorizontalAlignment === TextInput.AlignRight
         anchors.left: parent.left
         anchors.leftMargin: Kirigami.Units.smallSpacing
         anchors.top: parent.top
@@ -182,7 +182,6 @@ QQC2.TextField {
         padding: Kirigami.Units.smallSpacing
         spacing: Kirigami.Units.smallSpacing
         layoutDirection: Qt.RightToLeft
-        LayoutMirroring.enabled: root.effectiveHorizontalAlignment === TextInput.AlignRight
         anchors.right: parent.right
         anchors.rightMargin: Kirigami.Units.smallSpacing
         anchors.top: parent.top

src/controls/SearchField.qml
@@ -59,12 +59,12 @@ Kirigami.ActionTextField {
     property bool delaySearch: false
 
     // padding to accommodate search icon nicely
-    leftPadding: if (effectiveHorizontalAlignment === TextInput.AlignRight) {
+    leftPadding: if (Qt.application.layoutDirection === Qt.RightToLeft) {
         return _rightActionsRow.width + Kirigami.Units.smallSpacing
     } else {
         return searchIcon.width + Kirigami.Units.smallSpacing * 3
     }
-    rightPadding: if (effectiveHorizontalAlignment === TextInput.AlignRight) {
+    rightPadding: if (Qt.application.layoutDirection === Qt.RightToLeft) {
         return searchIcon.width + Kirigami.Units.smallSpacing * 3
     } else {
         return _rightActionsRow.width + Kirigami.Units.smallSpacing
@@ -93,8 +93,7 @@ Kirigami.ActionTextField {
     EnterKey.type: Qt.EnterKeySearch
     rightActions: [
         Kirigami.Action {
-            //ltr confusingly refers to the direction of the arrow in the icon, not the text direction which it should be used in
-            icon.name: root.effectiveHorizontalAlignment === TextInput.AlignRight ? "edit-clear-locationbar-ltr" : "edit-clear-locationbar-rtl"
+            icon.name: root.LayoutMirroring.enabled ? "edit-clear-locationbar-ltr" : "edit-clear-locationbar-rtl"
             visible: root.text.length > 0
             text: qsTr("Clear search")
             onTriggered: {